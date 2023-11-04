555f881ee62c2fba4fac2c32edb55111921728ee
たすくん <nizi.minato@gmail.com>
2023-11-05 03:47:06 +0900

    update: change resource

--- bnscup2023/src/Main.cpp
+++ bnscup2023/src/Main.cpp
@@ -16,17 +16,17 @@ void Main() {
 
 	Scene::SetBackground(theme.background);
 
-	constexpr char32 DefaultFontPathName[] = U"resource/font/makinas4/Makinas-4-Square.otf";
+	auto fontPath = Resource(U"resource/font/makinas4/Makinas-4-Square.otf");
 
-	FontAsset::Register(U"UI.Title", FontMethod::SDF, 90, DefaultFontPathName);
-	FontAsset::Register(U"UI.Normal", FontMethod::SDF, 48, DefaultFontPathName);
-	FontAsset::Register(U"UI.Small", FontMethod::SDF, 24, DefaultFontPathName);
+	FontAsset::Register(U"UI.Title", FontMethod::SDF, 90, fontPath);
+	FontAsset::Register(U"UI.Normal", FontMethod::SDF, 48, fontPath);
+	FontAsset::Register(U"UI.Small", FontMethod::SDF, 24, fontPath);
 
-	AudioAsset::Register(U"Global.BGM", U"resource/sounds/bgm.mp3", Loop::Yes);
-	AudioAsset::Register(U"Game.PickupItem", U"resource/sounds/pickup-item.mp3");
-	AudioAsset::Register(U"Game.Hit.Blackhole", U"resource/sounds/hit-blackhole.mp3");
-	AudioAsset::Register(U"Game.Player.Damaged", U"resource/sounds/player.damaged.mp3");
-	AudioAsset::Register(U"Game.Levelup", U"resource/sounds/levelup.mp3");
+	AudioAsset::Register(U"Global.BGM", Resource(U"resource/sounds/bgm.mp3"), Loop::Yes);
+	AudioAsset::Register(U"Game.PickupItem", Resource(U"resource/sounds/pickup-item.mp3"));
+	AudioAsset::Register(U"Game.Hit.Blackhole", Resource(U"resource/sounds/hit-blackhole.mp3"));
+	AudioAsset::Register(U"Game.Player.Damaged", Resource(U"resource/sounds/player.damaged.mp3"));
+	AudioAsset::Register(U"Game.Levelup", Resource(U"resource/sounds/levelup.mp3"));
 
 	// シーンのあれこれ
 	App manager;
