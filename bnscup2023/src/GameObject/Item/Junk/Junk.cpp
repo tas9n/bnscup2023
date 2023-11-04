b6dbc810ebc0181f856d0e8ab068b08393abf167
たすくん <nizi.minato@gmail.com>
2023-11-05 03:58:27 +0900

    remove: Junk::draw() -> elapsed

--- bnscup2023/src/GameObject/Item/Junk/Junk.cpp
+++ bnscup2023/src/GameObject/Item/Junk/Junk.cpp
@@ -12,8 +12,6 @@ void Junk::update() {
 void Junk::draw() const {
 	const ScopedRenderStates2D blend{ BlendState::Additive };
 
-	double elapsed = timer.sF();
-
 	auto tex = texture.scaled(scale);
 	tex.rotated(Periodic::Sine1_1(1s, Scene::Time()) * 15_deg).drawAt(pos, ColorF{ Palette::White, GameObject::opacity });
 	tex.scaled(1.1).rotated(Periodic::Sine1_1(1s, Scene::Time()) * 15_deg).drawAt(pos, ColorF{ Palette::White, .5 * GameObject::opacity });
